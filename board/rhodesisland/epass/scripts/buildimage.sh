af292c463f9d6e514fc442f838280bb6706f063d
Shirogane Tsumugi <inapp@iccmc.cc>
2025-12-22 02:46:52 +0800

    changed rootfs compress method to lzo.

--- board/rhodesisland/epass/scripts/buildimage.sh
+++ board/rhodesisland/epass/scripts/buildimage.sh
@@ -6,7 +6,7 @@ fakeroot sh -c "rm ubi.img"
 mkdir rootfs
 fakeroot sh -c "tar xvf rootfs.tar -C rootfs/"
 
-fakeroot sh -c "mkfs.ubifs -x none -F -m 2048 -e 126976 -c 732 -o rootfs_ubifs.img -r rootfs"
+fakeroot sh -c "mkfs.ubifs -x lzo -F -m 2048 -e 126976 -c 732 -o rootfs_ubifs.img -r rootfs"
 fakeroot sh -c "ubinize -o ubi.img -m 2048 -p 131072 -O 2048 -s 2048 ./ubinize.cfg  -v"
 
 rm ubinize.cfg
