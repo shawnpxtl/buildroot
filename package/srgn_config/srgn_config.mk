799d711a483c1dea7279ff87a6507857c94113db
Shirogane Tsumugi <inapp@iccmc.cc>
2026-01-19 06:22:15 +0800

    added srgn_config

---
+++ package/srgn_config/srgn_config.mk
@@ -0,0 +1,9 @@
+SRGN_CONFIG_VERSION = V0.1
+SRGN_CONFIG_SITE = https://github.com/rhodesepass/srgn_config.git
+SRGN_CONFIG_SITE_METHOD = git
+
+define SRGN_CONFIG_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/srgn_config $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(cmake-package))
