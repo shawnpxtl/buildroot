b180cb586a02f54f404385535a91d4458528e2b7
Shirogane Tsumugi <inapp@iccmc.cc>
2026-01-20 15:55:15 +0800

    corrected srgn_config format.

--- package/srgn_config/srgn_config.mk
+++ package/srgn_config/srgn_config.mk
@@ -1,3 +1,10 @@
+################################################################################
+#
+# srgn_config
+#
+################################################################################
+
+
 SRGN_CONFIG_VERSION = V0.1
 SRGN_CONFIG_SITE = https://github.com/rhodesepass/srgn_config.git
 SRGN_CONFIG_SITE_METHOD = git
