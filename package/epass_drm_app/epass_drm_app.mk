afd2e4d0818a53c029c7f64a6b4860d42702e1df
Shirogane Tsumugi <inapp@iccmc.cc>
2026-01-23 10:43:16 +0800

    fix build problem

--- package/epass_drm_app/epass_drm_app.mk
+++ package/epass_drm_app/epass_drm_app.mk
@@ -5,10 +5,10 @@
 ################################################################################
 
 
-EPASS_DRM_APP_VERSION = a2.4.1
+EPASS_DRM_APP_VERSION = a0b1c0b148a2a4d0ba1d8222530aa7579607ce14
 EPASS_DRM_APP_SITE = https://github.com/rhodesepass/drm_app_neo.git
 EPASS_DRM_APP_SITE_METHOD = git
-EPASS_DRM_APP_DEPENDENCIES = libcedarx libcedarc libdrm
+EPASS_DRM_APP_DEPENDENCIES = libcedarx libcedarc libdrm libpng libevdev jpeg-turbo
 EPASS_DRM_APP_GIT_SUBMODULES = YES
 EPASS_DRM_APP_CONF_OPTS = -DBUILD_SHARED_LIBS=OFF
 
